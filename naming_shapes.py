a6d517b3c2acb652f2ba4daf866f2f8e69087925
eurbin5 <eurbin5@illinois.edu>
2026-04-06 23:54:31 -0500

    Create naming_shapes.py

---
+++ naming_shapes.py
@@ -0,0 +1,18 @@
+def load_shapes():
+    pass
+
+def collect_responses():
+    pass
+
+def categorize_sound(response):
+    """Categorizes the sounds partipipants typed according to the phonotactic rules of English (basically IPA transcription)"""
+    pass
+
+def save_results():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
